ea15d3454da4b1ef76aa980b97c0b1eb5f827cad
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-06 18:28:09 -0300

    Create variaveisEConstantes.js

---
+++ FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,23 @@
+
+// var ou let para criar variaveis
+// let nao pode trocar o valor da variavel
+
+
+var a = 1;
+let b = 4;
+
+var a = 30
+b = 40;
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log (a,b)
+
+// const se utiliza quando na sua aplicacao um numero que nao precisa ser mudando entao melhor utilizar const.
+const c = 5
+// c = 50
+console.log(c)
+
+
