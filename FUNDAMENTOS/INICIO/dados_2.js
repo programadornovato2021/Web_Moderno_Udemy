ab134d840fcc071fc5abeca389daa9d3c2ea3978
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-06 16:29:48 -0300

    Update dados_2.js

--- FUNDAMENTOS/INICIO/dados_2.js
+++ FUNDAMENTOS/INICIO/dados_2.js
@@ -6,6 +6,14 @@ let desconto = 0.4;
 let precoComDesconto = preco * (1 - desconto);
  console.log (precoComDesconto);
 
+ // let para criar variaveis , jeito correto e atualizado!
+ let nome = "Caderno"; //Sequencia de simbolos , letras
+ let categoria = "Papelaria"
+ console.log ("Produto: " + nome
+ + ", Categoria: " + categoria
+ +", Preço" + preco
+ + ", Desconto: " + desconto);
 
+ console.log(1+1);
 
 
