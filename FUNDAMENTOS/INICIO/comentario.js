99499ae993866f8116ea9ffa6e108b96bc9b7719
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-06 11:08:03 -0300

    Create comentario.js

---
+++ FUNDAMENTOS/INICIO/comentario.js
@@ -0,0 +1,12 @@
+
+// console.log é usado para ixibir algo na tela do computador!
+console.log('Passo #01');
+console.log('Passo #02'); // mais um exemplo de console.log!
+
+
+/*
+Esse é um comentario de multiplas linhas!
+
+*/
+
+console.log('Passo #03');
