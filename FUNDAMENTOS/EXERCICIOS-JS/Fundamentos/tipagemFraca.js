a5fc84c725584600b00d9db0bc98f3195c4c8a7e
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-06 18:35:49 -0300

    Create tipagemFraca.js

---
+++ FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = "Legal";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// typof ajuda descobrir o tipo , exemplo number, string, boleam
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nome genericos e siglas
+let valor = ''
+let numero = 1
+
+// Evite coisas desse tipo
+let pqp = false  // Produto quimico Perigoso...kkk
