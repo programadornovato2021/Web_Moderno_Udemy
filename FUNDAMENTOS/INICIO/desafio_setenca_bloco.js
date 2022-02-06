0915b9715ba6bfbdf3a6af034758bcbb21529104
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-06 11:09:15 -0300

    Create desafio_setenca_bloco.js

---
+++ FUNDAMENTOS/INICIO/desafio_setenca_bloco.js
@@ -0,0 +1,40 @@
+/*
+  ||exemplo da estrutura||
+// Bloco 1
+1
+3
+5
+7
+9
+
+// Bloco 2
+2
+4
+6
+8
+10
+
+
+*/
+
+
+
+
+ // Numeros pares - Bloco 01
+ {
+    console.log( " #01")
+    console.log( " #03")
+    console.log( " #05")
+    console.log( " #07")
+    console.log( " #09")
+}
+// Numeros impares - Bloco 02
+{
+    console.log( " #02")
+    console.log( " #04")
+    console.log( " #06")
+    console.log( " #08")
+    console.log( " #10")
+
+
+}
