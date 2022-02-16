42a0ca9823e7ffbeeb7e2493b8bde80e4fe17f74
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-16 17:28:01 -0300

    boolean

--- FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/string.js
+++ FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/string.js
@@ -1,8 +1,8 @@
 const escola = "Cod3r"
 
-console.log(escola.charAt(4))
+console.log(escola.charAt(4))  //vai mostrar a letra 'r' no console 
 console.log(escola.charAt(5))
-console.log(escola.charCodeAt(3))
+console.log(escola.charCodeAt(3)) //
 console.log(escola.indexOf('3'))
 
 
@@ -12,4 +12,4 @@ console.log(escola.substring(0,3))
 
 console.log('Escola'.concat(escola).concat('!'))
 console.log(escola.replace(3, 'e'))
-console.log ('Ana, Maria,Pedro'.split(','))
+console.log ('Ana, Maria,Pedro'.split(',')) // gerou um array com 3 elementos separados
