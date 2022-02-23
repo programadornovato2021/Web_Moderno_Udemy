422ea55fceccac3168c02cfd7aaeb5e371ab5756
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-02-23 19:56:32 -0300

    Create usandoVar1.js

---
+++ FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/usandoVar1.js
@@ -0,0 +1,33 @@
+{
+
+    {
+
+           {
+
+
+
+                {
+
+                    var sera  = 'Sera???'
+                    console.log(sera)
+                }
+
+
+           } 
+
+
+
+    }
+
+
+}
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local)
