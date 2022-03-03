875c7a1dfcc4833cc41fd827d154abb5383060d7
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-03 16:39:59 -0300

    novo recurso

---
+++ FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/atribuicao.js
@@ -0,0 +1,18 @@
+//ATRIBUICAO
+
+const a = 7 // atribuindo o valor de 7 a letra a
+let b = 3
+
+b += a // b = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console . log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
