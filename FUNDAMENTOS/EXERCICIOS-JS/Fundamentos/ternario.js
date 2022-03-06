70d235bf1a149f46a91d03a3f7a041149ccdf07e
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-06 14:25:19 -0300

    tratamento de erro

--- FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/ternario.js
+++ FUNDAMENTOS/EXERCICIOS-JS/Fundamentos/ternario.js
@@ -1,3 +1,5 @@
-const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'   // 
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'   // operador ternario e retornar um resultado verdadeiro ou falso  operando + verdadeiro ou falso
 console.log(resultado(7.1))
 console.log(resultado(6.1))
+
+
