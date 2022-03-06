d3eae22e2f901442c14be7c0e04470a458812948
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-06 14:37:45 -0300

    Create it1.js

---
+++ FUNDAMENTOS/EXERCICIOS-JS/EstruturasJs/it1.js
@@ -0,0 +1,25 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){ // if =  se a nota for maior ou igual a 7
+        console.log('Aprovado com ' + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log('É verdade... ' + valor)
+    }
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 2])
+seForVerdadeEuFalo({})
