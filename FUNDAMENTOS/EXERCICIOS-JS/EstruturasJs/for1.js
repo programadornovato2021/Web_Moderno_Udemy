9b8297ded3d4b986f7cf77542765d969ac570351
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-16 23:27:32 -0300

    Create for1.js

---
+++ FUNDAMENTOS/EXERCICIOS-JS/EstruturasJs/for1.js
@@ -0,0 +1,15 @@
+let contator = 1  // declaracao
+while(contator <= 10) {  //expressao
+    console.log(`contador = ${contator}`)
+    contator++
+}
+
+
+for(let i = 1; i <= 10; i ++){  
+    console.log(`i = ${i}`)
+}
+
+const notas = [ 6.7, 7.4, 9.8, 8.1, 7.7]
+for(i = 0; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`)
+}
