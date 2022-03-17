29f4380bdd8a8cbb661a23dc8e2bf4fcd25a0da7
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-16 23:10:36 -0300

    Create while.js

---
+++ FUNDAMENTOS/EXERCICIOS-JS/EstruturasJs/while.js
@@ -0,0 +1,15 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random()* (max-min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+    while(opcao != -1) {
+        opcao = getInteiroAleatorioEntre(-1, 10)
+        console.log(`opção escolhida foi ${opcao}.`)
+    }
+    
+console.log('Até a proxima')
+
+// executa uma quantidade de vezes até que o valor seja o mesmo escolhido na expressão  
